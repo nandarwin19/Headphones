f748467da0b3e20d23b318a45e6cdadaabf88aad
nwin19 <nandarwinbuma@gmail.com>
2024-03-03 14:15:00 +0630

    remove unnecessary and change cursor colors

--- src/components/Others.jsx
+++ src/components/Others.jsx
@@ -155,7 +155,7 @@ const Others = () => {
       <div className="h-screen w-full">
         <video
           className="w-full h-full object-cover"
-          src="/other.mp4"
+          src="/video/other.mp4"
           autoPlay
           loop
           muted
